1dbe5d2d0eebf6456ca04005a36152e0dcb67a38
favibe <ibefavour85@gmail.com>
2025-10-05 18:31:47 +0100

    Error Message

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 let formData = document.querySelector(".form");
 let submitButton = document.querySelector(".button");
 
+//
+let errorMessages = document.querySelectorAll(".error-message");
+let emptyfieldMessages = document.querySelectorAll(".empty-field");
 
 //STORING THE VALUES INTO A VARIABLE
 let firstName, lastName, email, password;
-let field; // whee we store
+let field; // whee we store THE TARGET(input key)
+
+//Regular express
+
+
+//
+for (let errorMessage of errorMessages){
+  errorMessage.classList.add("d-none");
+}
+
+for (let emptyfieldMessage of emptyfieldMessages){
+  emptyfieldMessage.classList.add("d-none");
+}
+
 
 formData.addEventListener("keyup", (event)=> {
     event.preventDefault();
